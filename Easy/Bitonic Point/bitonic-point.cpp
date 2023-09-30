7575a19b3eeba34bef1c3b21dfc78e7fd7a067fd
Deepak Patel <115283329+deepakpatel-7@users.noreply.github.com>
2023-09-30 11:40:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Bitonic Point/bitonic-point.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int findMaximum(int arr[], int n) {
+	    int low = 0;
+	    int high = n-1;
+	    
+	    while(low<=high){
+	        int mid = (low+high)/2;
+	        
+	        if( (mid==0 || arr[mid-1] <=arr[mid]) && (mid==n-1 || arr[mid+1]<=arr[mid])){
+	            return arr[mid];
+	        }
+	        
+	        if(mid>0 && arr[mid-1] >=arr[mid]){
+	            high = mid-1;
+	        }
+	        
+	        else{
+	            low = mid +1;
+	        }
+	    }
+	    return -1;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.findMaximum(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
