42b6680f7739970096cdaf7cbb8fc7bc90b4fbf9
Deepak Patel <115283329+deepakpatel-7@users.noreply.github.com>
2024-04-26 18:57:05 +0530

    Updated Solution - GfG to GitHub

--- Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
+++ Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
@@ -8,31 +8,32 @@ using namespace std;
 class Solution{
 public:	
 	int search(int A[], int N){
-	    
-        if(N==1) return A[0];
+	     int n  = N;
+        if(n==1) return A[0];
+        if(A[0]!=A[1]) return A[0];
+        if(A[n-1]!=A[n-2]) return A[n-1];
 
-        if(A[0] != A[1]) return A[0];
-        if(A[N-1] != A[N-2]) return A[N-1];
-
-        int low= 1 , high = N-2;
+        int low = 1 ; 
+        int high = n-2;
 
         while(low<=high){
-            int mid = (low+high) /2;
+            int mid = (low+high)/2;
 
-            if(A[mid] != A[mid+1] && A[mid] != A[mid-1]){
+            if(A[mid] != A[mid+1] && A[mid]!=A[mid-1]){
                 return A[mid];
             }
-
-            if((mid%2==1 && A[mid]==A[mid-1]) || (mid%2 == 0 && A[mid]==A[mid+1])){
+            //left half
+            if((mid%2==1 && A[mid]==A[mid-1]) || (mid%2==0 && A[mid]==A[mid+1])){
                 low = mid+1;
-
             }
-
             else{
                 high = mid-1;
             }
         }
+
         return -1;
+
+
 	    
 	}
 };
